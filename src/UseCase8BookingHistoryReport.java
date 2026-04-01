961d809904d459b72cdddad5a8df90c6d3cfee92
Mutthuram S R <srmutthuram2007@gmail.com>
2026-04-01 08:44:14 +0530

    Add files via upload

---
+++ src/UseCase8BookingHistoryReport.java
@@ -0,0 +1,17 @@
+public class UseCase8BookingHistoryReport {
+
+    public static void main(String[] args) {
+
+        BookingHistory history = new BookingHistory();
+
+        history.addReservation(new Reservation("Abhi", "Single"));
+        history.addReservation(new Reservation("Subha", "Double"));
+        history.addReservation(new Reservation("Vanmathi", "Suite"));
+
+        BookingReportService reportService = new BookingReportService();
+
+        System.out.println("Booking History and Reporting\n");
+
+        reportService.generateReport(history);
+    }
+}
